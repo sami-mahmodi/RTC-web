a67ccd3d0e6e70b8c0f7161c9c5ecaf814fc6973
Samiullah Mahmodi <113135377+sami-mahmodi@users.noreply.github.com>
2024-07-05 00:01:03 -0400

    logo and the assets added

--- rtc-web/src/components/Header.jsx
+++ rtc-web/src/components/Header.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from 'react';
+import './header.scss'; // Ensure correct path if SCSS is not directly in the same directory
+import Link from 'next/link';
+import Image from 'next/image';
+
+const paths = [
+  {
+    linkName: 'Home',
+    linkPath: '/'
+  },
+  {
+    linkName: 'About',
+    linkPath: '/about'
+  },
+  {
+    linkName: 'Contact',
+    linkPath: '/contact'
+  }
+];
+
+const LinkRoute = ({ path, pathName }) => (
+  <Link href={path} >
+    <div className="link">{pathName}</div>
+  </Link>
+);
 
 function Header() {
   return (
-    <div className='wrapper'>
-        Welcome
+    <div className="wrapper">
+        <div>
+         <Image src='/assets/rtc/LOGO.png' width={40} height={40}></Image>
+        </div>
+        <div>
+
+      {paths.map((pathObj, index) => (
+          <LinkRoute key={index} path={pathObj.linkPath} pathName={pathObj.linkName} />
+          ))}
+          </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
